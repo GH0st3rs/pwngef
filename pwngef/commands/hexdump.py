c6c3021d2dfc975352e16b886b9d3da828de421b
GH0st3rs <GH0st3rs@users.noreply.github.com>
2019-03-14 15:29:37 +0300

    Add command hexdump

---
+++ pwngef/commands/hexdump.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import pwngef.commands
+from pwngef.commands import GenericCommand
+from pwngef.color import message
+import pwngef.proc
+import pwngef.hexdump
+import pwngef.memory
+
+
+@pwngef.commands.register_command
+class HexDumpCommand(GenericCommand):
+    '''Hexdumps data at the specified address (or at $sp)'''
+    _cmdline_ = "hexdump"
+    _syntax_ = "{:s} [address] [count]".format(_cmdline_)
+    _example_ = "\n{0:s}\n{0:s} 0x10000 100".format(_cmdline_)
+
+    def __init__(self):
+        super(HexDumpCommand, self).__init__()
+        self.add_setting("hexdump_width", 16, "line width of hexdump command")
+        self.add_setting('hexdump_bytes', 64, 'number of bytes printed by hexdump command')
+        self.add_setting('hexdump_colorize_ascii', True, 'whether to colorize the hexdump command ascii section')
+        self.add_setting('hexdump_ascii_block_separator', '│', 'block separator char of the hexdump command')
+        self.offset = 0
+        self.last_address = 0
+
+    @pwngef.proc.OnlyWhenRunning
+    def do_invoke(self, argv):
+        width = self.get_setting('hexdump_width')
+        if not argv:
+            message.warn("No arguments found! Default dump 128 byte by $sp")
+            address = int(pwngef.arch.CURRENT_ARCH.sp)
+            count = 128
+        else:
+            count = int(argv[1]) if len(argv) > 1 else 128
+            address = int(argv[0], 16) if argv[0].startswith('0x') or argv[0].startswith('0X') else int(argv[0])
+
+        data = pwngef.memory.read(address, count, partial=True)
+        for _, line in enumerate(pwngef.hexdump.hexdump(data, address=address, width=width)):
+            print(line)
+        return None
---
+++ pwngef/hexdump.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+"""
+Hexdump implementation, ~= stolen from pwntools.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import copy
+import string
+
+import pwngef.config
+import pwngef.color.hexdump as H
+
+color_scheme = None
+printable = None
+
+
+def groupby(array, count, fill=None):
+    array = copy.copy(array)
+    while fill and len(array) % count:
+        array.append(fill)
+    for i in range(0, len(array), count):
+        yield array[i:i + count]
+
+
+def load_color_scheme():
+    global color_scheme, printable
+    #
+    # We want to colorize the hex characters and only print out
+    # printable values on the righ hand side.
+    #
+    color_scheme = {i: H.normal("%02x" % i) for i in range(256)}
+    printable = {i: H.normal('.') for i in range(256)}
+    # load config
+    config_colorize_ascii = pwngef.config.get('hexdump.hexdump_colorize_ascii')
+
+    for c in bytearray((string.ascii_letters + string.digits + string.punctuation).encode('utf-8', 'ignore')):
+        color_scheme[c] = H.printable("%02x" % c)
+        printable[c] = H.printable("%s" % chr(c)) if config_colorize_ascii else "%s" % chr(c)
+
+    for c in bytearray(b'\x00'):
+        color_scheme[c] = H.zero("%02x" % c)
+        printable[c] = H.zero('.') if config_colorize_ascii else '.'
+
+    for c in bytearray(b'\xff\x7f\x80'):
+        color_scheme[c] = H.special("%02x" % c)
+        printable[c] = H.special('.') if config_colorize_ascii else '.'
+
+    color_scheme[-1] = '  '
+    printable[-1] = ' '
+
+
+def hexdump(data, address=0, width=16, skip=True, offset=0):
+    # load config
+    config_separator = pwngef.config.get('hexdump.hexdump_ascii_block_separator')
+    if not color_scheme or not printable:
+        load_color_scheme()
+    data = list(bytearray(data))
+    base = address
+    last_line = None
+    skipping = False
+    for i, line in enumerate(groupby(data, width, -1)):
+        if skip and line == last_line:
+            if not skipping:
+                skipping = True
+                yield '...'
+            continue
+        else:
+            skipping = False
+            last_line = line
+
+        hexline = []
+
+        if address:
+            hexline.append(H.offset("+%04x " % ((i + offset) * width)))
+
+        hexline.append(H.address("%#08x  " % (base + (i * width))))
+
+        for group in groupby(line, 4):
+            for char in group:
+                hexline.append(color_scheme[char])
+                hexline.append(' ')
+            hexline.append(' ')
+
+        hexline.append(H.separator('%s' % config_separator))
+        for group in groupby(line, 4):
+            for char in group:
+                hexline.append(printable[char])
+            hexline.append(H.separator('%s' % config_separator))
+
+        yield(''.join(hexline))
+
+    # skip empty footer if we printed something
+    if last_line:
+        return
+
+    hexline = []
+
+    if address:
+        hexline.append(H.offset("+%04x " % len(data)))
+
+    hexline.append(H.address("%#08x  " % (base + len(data))))
+
+    yield ''.join(hexline)
